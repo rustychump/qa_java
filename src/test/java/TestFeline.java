677a8a242ea24f6150064d111d5339fd0ee145d6
Aleksandr Marenkov <a.maryenkov@yandex.ru>
2022-08-30 19:46:06 +0300

    add a mock to the Feline class

--- src/test/java/TestFeline.java
+++ src/test/java/TestFeline.java
@@ -1,17 +1,20 @@
 import com.example.Feline;
 import org.junit.Assert;
 import org.junit.Test;
+import org.mockito.Mockito;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class TestFeline {
-
     @Test
     public void checkFeline() throws Exception {
         Feline feline = new Feline();
-        Assert.assertEquals(List.of("Животные", "Птицы", "Рыба"), feline.eatMeat());
-        Assert.assertEquals("Кошачьи", feline.getFamily());
-        Assert.assertEquals(1, feline.getKittens());
-        Assert.assertEquals(2, feline.getKittens(2));
+        Feline felineSpy = Mockito.spy(feline);
+        Mockito.when(felineSpy.getFood("Хищник")).thenReturn(new ArrayList<>());
+        Assert.assertEquals(new ArrayList<>(), felineSpy.eatMeat());
+        Assert.assertEquals("Кошачьи", felineSpy.getFamily());
+        Assert.assertEquals(1, felineSpy.getKittens());
+        Assert.assertEquals(2, felineSpy.getKittens(2));
     }
 }
